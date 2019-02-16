3cc867b44502b95ac5b7eca95e3ddbe1cd3b16e6
unparalleled-js <yingthi@live.com>
2019-02-16 16:25:37 -0700

    Changed language to validator

--- src/components/lcas/index.jsx
+++ src/components/lcas/index.jsx
@@ -14,7 +14,7 @@ const LCAItem = (addr, v) => <LCA address={addr} verified={v} />;
 
 const LCAs = () => (
   <div className={LCAsClasses()}>
-    <h3 className={titleClasses()}>Unverified</h3>
+    <h3 className={titleClasses()}>Unvalidated</h3>
     <hr />
     <Row className={rowClasses()}>
       <Col xs={3}>
@@ -23,7 +23,7 @@ const LCAs = () => (
         {LCAItem(mockLCAs[2].address, mockLCAs[2].verified)}
       </Col>
     </Row>
-    <h3 className={titleClasses()}>Verified</h3>
+    <h3 className={titleClasses()}>Validated/h3>
     <hr />
     <Row className={rowClasses()}>
       <Col xs={3}>
