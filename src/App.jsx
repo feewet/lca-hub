72adb557bc5ad2e26d61469342f3004024cc7769
unparalleled-js <yingthi@live.com>
2019-02-16 13:19:08 -0700

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LCAs from 'components/lcas';
 import NavBar from 'components/nav-bar';
 import { hot } from 'react-hot-loader';
@@ -9,7 +9,9 @@ import Style from '../src/styles.css';
 const App = () => (
   <div>
     <NavBar />
-    <LCAs />
+    <Switch>
+      <Route path="/" exact component={LCAs} />
+    </Switch>
   </div>
 );
 
