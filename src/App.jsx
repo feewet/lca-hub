957794a24ba404609e0d1aadec9382ebe621e371
unparalleled-js <yingthi@live.com>
2019-02-15 20:47:37 -0700

    Template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+import { hot } from 'react-hot-loader';
+
+const App = () => (
+  <div>TEMPLATE</div>
+);
+
+export default hot(module)(App);
