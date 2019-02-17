7c65631aa600ded24be1538c5e4b838207f58537
unparalleled-js <yingthi@live.com>
2019-02-16 21:20:10 -0700

    Added home page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import homeStyles from './home.css';
 
-const Home = () => <div />;
+const paragraphClasses = () => `${homeStyles.Paragraph}`;
+
+const headingClasses = () => `${homeStyles.Heading}`;
+
+const linkClasses = () => `${homeStyles.Link}`;
+
+const linkRowClasses = () => `${homeStyles.LinkRow}`;
+
+const Home = () => (
+  <div>
+    <Row>
+      <Col xs={12}>
+        <h2 className={headingClasses()}>Create LCA Bounties</h2>
+        <h2 className={headingClasses()}>Solve Problems & Get Paid</h2>
+        <p className={paragraphClasses()}>
+          A Life Cycle Assessment quantifies the environmental impact of a
+          product or service. Organizations use LCAs to inform decisions
+          regarding sustainability and implementation of sustainable policies.
+          The current system to create LCAs is heavily centralized. Both
+          organizations and engineers can benefit greatly from a direct system
+          to draft and verify life cycle assessments. Currently, engineers must
+          work through a third party such as a government or corporation in
+          order to complete these reports. By removing the middle men,
+          assessments can be done significantly faster at lower cost.
+        </p>
+        <Link
+          className={linkClasses()}
+          style={{ left: '30%' }}
+          to="/request-bounty"
+        >
+          Create Bounty
+        </Link>
+        <Link className={linkClasses()} style={{ left: '50%' }} to="/lcas">
+          View Bounties
+        </Link>
+      </Col>
+    </Row>
+  </div>
+);
 
 export default Home;
